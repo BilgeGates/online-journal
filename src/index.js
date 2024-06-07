395116fff2aae88d27ad891022df8c7d6bf14143
BilgeGates <huseynzadekhatai@gmail.com>
2024-06-07 22:44:26 +0400

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
+import { ContextProvider } from "./contexts/ContextProvider";
+
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById("root")
+);
