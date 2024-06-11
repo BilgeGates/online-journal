8f5c2858bb01d22604dffa9769dc2bada62bf6cc
BilgeGates <huseynzadekhatai@gmail.com>
2024-06-11 22:55:50 +0400

    updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,8 @@
-import React from "react";
-import { useStateContext } from "../contexts/ContextProvider";
+import React from 'react';
 
-const Button = ({
-  icon,
-  bgColor,
-  color,
-  bgHoverColor,
-  size,
-  text,
-  borderRadius,
-  width,
-}) => {
+import { useStateContext } from '../contexts/ContextProvider';
+
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
   const { setIsClicked, initialState } = useStateContext();
 
   return (
