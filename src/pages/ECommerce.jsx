84c8e7c014ac656a6b5ae2e970fd464c39b44806
BilgeGates <huseynzadekhatai@gmail.com>
2024-06-11 22:59:25 +0400

    complated

--- src/pages/ECommerce.jsx
+++ src/pages/ECommerce.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { BsCurrencyDollar, BsDot } from "react-icons/bs";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
 import { IoIosMore } from "react-icons/io";
 import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
-import { Stacked, Pie, Button, LineCharts, SparkLine } from "../components";
+import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
 import {
   earningData,
   medicalproBranding,
@@ -34,7 +35,7 @@ const Ecommerce = () => {
 
   return (
     <div className="mt-24">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
@@ -44,7 +45,7 @@ const Ecommerce = () => {
             <button
               type="button"
               style={{ backgroundColor: currentColor }}
-              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full p-4"
+              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
             >
               <BsCurrencyDollar />
             </button>
@@ -62,12 +63,12 @@ const Ecommerce = () => {
           {earningData.map((item) => (
             <div
               key={item.title}
-              className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+              className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl "
             >
               <button
                 type="button"
                 style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+                className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
               >
                 {item.icon}
               </button>
@@ -77,29 +78,32 @@ const Ecommerce = () => {
                   {item.percentage}
                 </span>
               </p>
-              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+              <p className="text-sm text-gray-400  mt-1">{item.title}</p>
             </div>
           ))}
         </div>
       </div>
-
       <div className="flex gap-10 flex-wrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
           <div className="flex justify-between">
             <p className="font-semibold text-xl">Revenue Updates</p>
             <div className="flex items-center gap-4">
               <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
-                <BsDot />
+                <span>
+                  <GoPrimitiveDot />
+                </span>
                 <span>Expense</span>
               </p>
               <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
-                <BsDot />
+                <span>
+                  <GoPrimitiveDot />
+                </span>
                 <span>Budget</span>
               </p>
             </div>
           </div>
           <div className="mt-10 flex gap-10 flex-wrap justify-center">
-            <div className="border-r-1 border-color m-4 pr-10">
+            <div className=" border-r-1 border-color m-4 pr-10">
               <div>
                 <p>
                   <span className="text-3xl font-semibold">$93,438</span>
@@ -140,10 +144,10 @@ const Ecommerce = () => {
         </div>
         <div>
           <div
-            className="rounded-2xl md:w-400 p-4 m-3"
+            className=" rounded-2xl md:w-400 p-4 m-3"
             style={{ backgroundColor: currentColor }}
           >
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center ">
               <p className="font-semibold text-white text-2xl">Earnings</p>
               <div>
                 <p className="text-2xl text-white font-semibold mt-8">
@@ -164,10 +168,9 @@ const Ecommerce = () => {
               />
             </div>
           </div>
-
           <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
             <div>
-              <p className="text-2xl font-semibold">$43,246</p>
+              <p className="text-2xl font-semibold ">$43,246</p>
               <p className="text-gray-400">Yearly sales</p>
             </div>
             <div className="w-40">
@@ -181,7 +184,6 @@ const Ecommerce = () => {
           </div>
         </div>
       </div>
-
       <div className="flex gap-10 m-4 flex-wrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl">
           <div className="flex justify-between items-center gap-2">
@@ -229,11 +231,10 @@ const Ecommerce = () => {
             <DropDown currentMode={currentMode} />
           </div>
           <div className="md:w-full overflow-auto">
-            <LineCharts />
+            <LineChart />
           </div>
         </div>
       </div>
-
       <div className="flex flex-wrap justify-center">
         <div className="md:w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
           <div className="flex justify-between">
@@ -245,7 +246,7 @@ const Ecommerce = () => {
               <IoIosMore />
             </button>
           </div>
-          <div className="mt-10">
+          <div className="mt-10 ">
             {weeklyStats.map((item) => (
               <div
                 key={item.title}
@@ -264,6 +265,7 @@ const Ecommerce = () => {
                     <p className="text-sm text-gray-400">{item.desc}</p>
                   </div>
                 </div>
+
                 <p className={`text-${item.pcColor}`}>{item.amount}</p>
               </div>
             ))}
@@ -354,10 +356,10 @@ const Ecommerce = () => {
             </button>
           </div>
           <div className="mt-10">
-            <img className="md:w-96 h-50" src={product9} alt="" />
+            <img className="md:w-96 h-50 " src={product9} alt="" />
             <div className="mt-8">
               <p className="font-semibold text-lg">React 18 coming soon!</p>
-              <p className="text-gray-400">By Johnathan Doe</p>
+              <p className="text-gray-400 ">By Johnathan Doe</p>
               <p className="mt-8 text-sm text-gray-400">
                 This will be the small description for the news you have shown
                 here. There could be some great info.
