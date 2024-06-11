25c8502c982189fbe07c029986d6a3294453b223
BilgeGates <huseynzadekhatai@gmail.com>
2024-06-11 22:55:43 +0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-
 import { ContextProvider } from "./contexts/ContextProvider";
-
+import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>,
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
